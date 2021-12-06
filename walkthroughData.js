1b79ef275ad3b8d3dda00c9fe77f171a51aec87c
afc2134 <85919290+AFC2134@users.noreply.github.com>
2021-12-05 19:55:14 -0600

    README.md

---
+++ walkthroughData.js
@@ -0,0 +1,23 @@
+
+// product put
+{
+	"product_name": "alex pjs",
+      "price": 200.00,
+      "stock": 3,
+      "tagIds": [1, 2, 3, 4]
+}
+// category put
+{
+    "category_name": "pjs"
+    }
+
+// delete route tags
+{
+	"id": 1
+}
+// put route tags
+{
+	"id": 2,
+	"tag_name": "hip-hop music"
+}
+// 
